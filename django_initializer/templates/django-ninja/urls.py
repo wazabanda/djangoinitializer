140468e4a832b7f4a9e0772b7be5c7cd4a7a1642
wazabanda <1wazabanda@gmail.com>
2024-08-25 19:39:44 +0200

    update to djangoninja urls template

--- django_initializer/templates/django-ninja/urls.py
+++ django_initializer/templates/django-ninja/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('',include('core.urls')),
+    path('admin/', admin.site.urls),
     path('api/',api.urls,name='api'),
 ]
 
